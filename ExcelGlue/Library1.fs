98fa4a8db0eddbac41cdda7eee5050b04d30579c
cdr021 <cdr021@@pm.me>
2021-05-05 22:25:12 +0800

    Added Excel DNA dependencies

--- ExcelGlue/Library1.fs
+++ ExcelGlue/Library1.fs
@@ -2,3 +2,10 @@
 
 type Class1() = 
     member this.X = "F#"
+
+module FOO =
+    open ExcelDna.Integration
+
+    [<ExcelFunction(Description="My first .NET function")>]
+    let HelloDna name =
+        "Hello " + name

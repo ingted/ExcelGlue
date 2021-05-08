e6db34331b3eace55d08834b4a78d3a982034e28
cdr021 <cdr021@@pm.me>
2021-05-08 15:28:55 +0800

    Added XlXl module.

---
+++ ExcelGlue/2000_xlxl.fs
@@ -0,0 +1,14 @@
+﻿namespace ExcelGlue
+
+module XlXl =
+    open System
+    open ExcelDna.Integration
+
+    [<ExcelFunction(Category="XL", Description="Equality of 2 xl-values (\'variants\').")>]
+    let x_eq 
+        ([<ExcelArgument(Description= "Value 1.")>] val1: obj) 
+        ([<ExcelArgument(Description= "Value 2.")>] val2: obj) 
+        : obj  =
+
+        (val1 = val2) |> box
+
